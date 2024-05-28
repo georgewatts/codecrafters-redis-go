2a28ae11a0f05a29d251cc81602ef04293396756
George Watts <george.watts@bgnevents.com>
2024-05-28 15:22:28 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,7 +6,49 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
+
+type Command struct {
+	numberElements int
+	elements       []string
+}
+
+func newSimpleString(str string) string {
+	return fmt.Sprintf("+%v\r\n", str)
+}
+
+func newBulkString(str string) string {
+	return fmt.Sprintf("$%d\r\n%v\r\n", len(str), str)
+}
+
+func pong() []byte {
+	return []byte("+PONG\r\n")
+}
+
+func echo(str string) []byte {
+	return []byte(newBulkString(str))
+}
+
+func parser(str string) ([]byte, error) {
+	command := strings.Split(str, "\r\n")
+	fmt.Printf("command: %v\n", command)
+
+	if command[0][0] != '*' {
+		fmt.Println("Not a valid command ig")
+	}
+
+	formattedCommand := strings.ToLower(command[2])
+
+	switch formattedCommand {
+	case "ping":
+		return pong(), nil
+	case "echo":
+		return echo(command[4]), nil
+	}
+
+	return nil, errors.New("Command unsupported")
+}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
@@ -15,7 +57,7 @@ func handleConnection(conn net.Conn) {
 
 	for {
 		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		count, err := conn.Read(buf)
 		if errors.Is(err, io.EOF) {
 			fmt.Println("Client closed the connections:", conn.RemoteAddr())
 			break
@@ -24,8 +66,17 @@ func handleConnection(conn net.Conn) {
 			os.Exit(1)
 		}
 
-		fmt.Println("Received: ", string(buf))
-		response := []byte("+PONG\r\n")
+		if count == 0 {
+			return
+		}
+
+		data := string(buf[:count])
+
+		response, err := parser(data)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
 		conn.Write(response)
 	}
 }
